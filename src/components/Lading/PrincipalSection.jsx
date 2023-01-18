aacf8392666c9899259e18a7d8fc687b237d2789
Luis <lmavendano97@outlook.es>
2023-01-18 16:16:38 -0500

    arreglos links

--- src/components/Lading/PrincipalSection.jsx
+++ src/components/Lading/PrincipalSection.jsx
@@ -1,55 +1,75 @@
-import React from 'react'
-import mujerImg from '../../assets/home-assets/mujer-principal-section.png'
-import logoRed from '../../assets/home-assets/icon-red.png'
+import React from "react";
+import mujerImg from "../../assets/home-assets/mujer-principal-section.png";
+import logoRed from "../../assets/home-assets/icon-red.png";
 
 const PrincipalSection = () => {
   return (
-    <div className='principal-section'>
-      <article className='article-principal-section'>
-        <h2 className='title-principal-section'>La investigación de mercados</h2>
-        <h3 className='subtitle-principal-section'>El amigo de las marcas</h3>
-        <p className='description-principal-section'>
-        Porque transformamos las opiniones en estrategias que maximizan las oportunidades de crecimiento, alcanzando tus indicadores de venta, consolidando tu organización y estableciendo estrategias focalizadas en el plan marketing; de esta manera se logra cumplir estándares de calidad, rapidez y confianza.
+    <div className="principal-section">
+      <article className="article-principal-section">
+        <h2 className="title-principal-section">
+          La investigación de mercados
+        </h2>
+        <h3 className="subtitle-principal-section">El amigo de las marcas</h3>
+        <p className="description-principal-section">
+          Porque transformamos las opiniones en estrategias que maximizan las
+          oportunidades de crecimiento, alcanzando tus indicadores de venta,
+          consolidando tu organización y estableciendo estrategias focalizadas
+          en el plan marketing; de esta manera se logra cumplir estándares de
+          calidad, rapidez y confianza.
         </p>
-        <button className='btn-principal-section'>Saber más</button>
+        <a
+          className="btn-principal-section"
+          href="/services/Investigacion de mercados.pdf"
+          target="_blank"
+        >
+          Saber más
+        </a>
       </article>
-      <div className='container-logo-principal-section'>
-        <img className='logo-principal-section' src={logoRed} alt="Logo red Background" />
+      <div className="container-logo-principal-section">
+        <img
+          className="logo-principal-section"
+          src={logoRed}
+          alt="Logo red Background"
+        />
       </div>
-      <div className='container-img-principal-section'>
-        <img className='img-principal-section' src={mujerImg} alt="mujer voceando" />
+      <div className="container-img-principal-section">
+        <img
+          className="img-principal-section"
+          src={mujerImg}
+          alt="mujer voceando"
+        />
       </div>
-      <ul className='container-statistics-principal-section'>
-        <li className='statistics-item'>
-          <div className='statistics-width'>
-            <h3 className='statistics-number'>500</h3>
-              <p className='statistics-description'>
-                Encuestas
-                <span className='statistics-marker'>Realizadas</span>
-              </p>
+      <ul className="container-statistics-principal-section">
+        <li className="statistics-item">
+          <div className="statistics-width">
+            <h3 className="statistics-number">500</h3>
+            <p className="statistics-description">
+              Encuestas
+              <span className="statistics-marker">Realizadas</span>
+            </p>
           </div>
         </li>
-        <li className='statistics-item'>
-          <div className='statistics-width'>
-            <h3 className='statistics-number'>15</h3>
-              <p className='statistics-description'>
-                Marcas
-                <span className='statistics-marker'>Trabajadas</span>
-              </p>
+        <li className="statistics-item">
+          <div className="statistics-width">
+            <h3 className="statistics-number">15</h3>
+            <p className="statistics-description">
+              Marcas
+              <span className="statistics-marker">Trabajadas</span>
+            </p>
           </div>
         </li>
-        <li className='statistics-item'>
-          <div className='statistics-width'>
-            <h3 className='statistics-number'>35K</h3>
-              <p className='statistics-description'>
-                Personas
-                <span className='statistics-marker'>Encuestadas</span>
-              </p>
+        <li className="statistics-item">
+          <div className="statistics-width">
+            <h3 className="statistics-number">35K</h3>
+            <p className="statistics-description">
+              Personas
+              <span className="statistics-marker">Encuestadas</span>
+            </p>
           </div>
         </li>
       </ul>
     </div>
-  )
-}
+  );
+};
 
-export default PrincipalSection
+export default PrincipalSection;
--- src/components/Lading/news-insights/OpinaInterview.jsx
+++ src/components/Lading/news-insights/OpinaInterview.jsx
@@ -1,16 +1,21 @@
-import React from 'react'
-import video from '../../../assets/opinapace/opinapace_video.png'
+import React from "react";
+import video from "../../../assets/opinapace/opinapace_video.png";
 
 const OpinaInterview = () => {
   return (
-    <section className='opina-interview'>
-      <h2 className='opina-interview__title'>Entrevista</h2>
-      <article className='opina-interview__container'>
-        <h3 className='opina-interview__container__title'>Canal 7 trujillo</h3>
-        <img className='opina-interview__video' src={video} alt="" />
+    <section className="opina-interview">
+      <h2 className="opina-interview__title">Entrevista</h2>
+      <article className="opina-interview__container">
+        <h3 className="opina-interview__container__title">Canal 7 trujillo</h3>
+        <a
+          href="https://www.facebook.com/watch/live/?ref=watch_permalink&v=6031736276842478"
+          target="_blank"
+        >
+          <img className="opina-interview__video" src={video} alt="" />
+        </a>
       </article>
     </section>
-  )
-}
+  );
+};
 
-export default OpinaInterview
+export default OpinaInterview;
